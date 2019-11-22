fcd2eed690dad62b237e680183d448a03299a022
Sinan <summerlover871@gmail.com>
2019-11-22 17:17:41 +0200

    Added some html and css files. Added HomeController.

---
+++ src/main/java/softuni/LionBet/web/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package softuni.LionBet.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String getIndex(){
+        return "home/index.html";
+    }
+}
